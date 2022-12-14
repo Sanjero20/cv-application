0ed13ef32663d291d79d426d9819dd24f271ce9e
Sanjero <gelosantos72@gmail.com>
2022-12-14 20:20:13 +0800

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import './styles/App.css';
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <Header></Header>
+      </div>
+    );
   }
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/Header.css';
+
+export class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>CV Creator</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
